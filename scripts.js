6679f9d4b28daf51759e616fc47ce8c3e3893586
Pawel <Pawel_lyko@yahoo.com>
2021-09-20 15:38:45 -0400

    mquery

--- scripts.js
+++ scripts.js
@@ -118,7 +118,19 @@ profBtn.onclick = (function(){
 
         } else {
             var signU = document.getElementById("Signup");
-            signU.style = ( "display: block; margin-top: 3vw; margin-left: 20vw;" );
+            var intViewportWidth = window.innerWidth;
+
+            var styleObjLarge = ( "display: block; margin-top: 3vw; margin-left: 20vw;" );
+
+            var styleObjSmall = ( "display: block; left: 0px; right: 0px; height: 97vh; margin-top: 2vh; width: 100vw; position: absolute;" );
+
+            console.log(intViewportWidth);
+
+            if(intViewportWidth > 880){
+                signU.style = styleObjLarge;
+            }else if(intViewportWidth <= 880){
+                signU.style = styleObjSmall;
+            }
             var meBoard = document.getElementById('myBoard');
             meBoard.style = ( "display: none;" );
             var tableC = document.getElementById("tableContainer");
@@ -137,70 +149,3 @@ profBtn.onclick = (function(){
 
 
 
-// (function ($) {
-//     "use strict"; // Start of use strict
-
-//     // Smooth scrolling using anime.js
-//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-//         if (
-//             location.pathname.replace(/^\//, "") ==
-//             this.pathname.replace(/^\//, "") &&
-//             location.hostname == this.hostname
-//         ) {
-//             var target = $(this.hash);
-//             target = target.length ?
-//                 target :
-//                 $("[name=" + this.hash.slice(1) + "]");
-//             if (target.length) {
-//                 anime({
-//                     targets: 'html, body',
-//                     scrollTop: target.offset().top - 72,
-//                     duration: 1000,
-//                     easing: 'easeInOutExpo'
-//                 });
-//                 return false;
-//             }
-//         }
-//     });
-
-//     //Closes responsive menu when a scroll trigger link is clicked
-//     $('.js-scroll-trigger').click(function () {
-//         $('.navbar-collapse').collapse('hide');
-//     });
-
-//   // Activate scrollspy to add active class to navbar items on scroll
-//    $('body').scrollspy({
-//     target: '#mainNav',
-//    offset: 75
-//    });
-
-//     //Collapse Navbar
-//     var navbarCollapse = function () {
-//         if ($("#mainNav").offset().top > 1) {
-//             $("#mainNav").addClass("navbar-scrolled");
-//         } else {
-//             $("#mainNav").removeClass("navbar-scrolled");
-//         }
-//     };
-//     // Collapse now if page is not at top
-//     navbarCollapse();
-//     // Collapse the navbar when page is scrolled
-//     $(window).scroll(navbarCollapse);
-
-//    // Magnific popup calls
-//     $('#portfolio').magnificPopup({
-//         delegate: 'a',
-//         type: 'image',
-//         tLoading: 'Loading image #%curr%...',
-//         mainClass: 'mfp-img-mobile',
-//         gallery: {
-//             enabled: true,
-//             navigateByImgClick: true,
-//             preload: [0, 1]
-//         },
-//         image: {
-//             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-//         }
-//     });
-
-// })(jQuery); // End of use strict
